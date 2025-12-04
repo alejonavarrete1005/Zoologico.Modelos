cbebbb3a4269850453a21ffa95eeaffb80c5983c
alejo10005 <alejonavarrete05@icloud.com>
2025-12-03 20:35:24 -0500

    program

--- Zoologico.API/Program.cs
+++ Zoologico.API/Program.cs
@@ -38,11 +38,12 @@ namespace Zoologico.API
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+
 
             app.UseHttpsRedirection();
 
